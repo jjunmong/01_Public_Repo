c18198fea14d431f51a7623e2190577eedb66e06
seolyh100456 <seolyh1004@naver.com>
2022-01-13 00:06:17 +0900

    ㅇ

--- 05_수집툴통합관리/test.py
+++ 05_수집툴통합관리/test.py
@@ -1,8 +1,23 @@
-import os
+# 리스트 s의 모든 요소에 제곱값을 가지는 새로운 리스트 생성 예제
+s = [1, 2, 3, 4, 5]
 
+# 1. 일반 파이썬 프로그래밍
+def make_square(s) :
+    r = []
+    for i in s :
+        r.append(i * i)
+    return r
 
-def b():
-    aa = r'C:\00_DEV\00_수집툴\04_기획팀_자체수집\자체수집\dist\10_폐교정보_0.1(20200214).exe'
-    os.startfile(aa)
+for i in make_square(s) :
+    print(i, end=' ')
+print()
 
-b()
+# 2. 함수를 일급 객체로 취급하는 map을 이용한 프로그래밍
+def square(x) :
+    return x * x
+
+m = map(square, s)    # map 객체는 함수, 리스트를 보관
+
+for i in m :
+    print(i, end=' ')
+print()
